9b71eb871552f9b7b7b2245aa5b0bf909cc62ebb
HetalRupareliya91 <43531538+HetalRupareliya91@users.noreply.github.com>
2022-12-01 12:54:25 +0530

    Updates

---
+++ precache-manifest.91a949b712c97ce845d123faf7377233.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "451987b3aca6c4c1c655",
+    "url": "/reacte_crud/static/css/main.baf08d78.chunk.css"
+  },
+  {
+    "revision": "451987b3aca6c4c1c655",
+    "url": "/reacte_crud/static/js/main.451987b3.chunk.js"
+  },
+  {
+    "revision": "2b1625471f38bbec6f5b",
+    "url": "/reacte_crud/static/css/1.46cab94b.chunk.css"
+  },
+  {
+    "revision": "2b1625471f38bbec6f5b",
+    "url": "/reacte_crud/static/js/1.2b162547.chunk.js"
+  },
+  {
+    "revision": "fcfe9eb13db6b890722b",
+    "url": "/reacte_crud/static/js/runtime~main.fcfe9eb1.js"
+  },
+  {
+    "revision": "6694349a64fca1aeb3b018b752038b22",
+    "url": "/reacte_crud/index.html"
+  }
+];
